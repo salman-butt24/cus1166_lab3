1f11ea5f8d5fc9ed8390acbf922c3e8c92868652
salmanbutt <salman.butt15@stjohns.edu>
2019-02-15 23:46:37 -0500

    fixed files

--- playground/app/routes.py
+++ playground/app/routes.py
@@ -1,6 +1,23 @@
 from app import app
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
+
+
+courses = [("Real Estate Finance", 73528), ("Moral Theology", 49264),
+           ("Software Engineering", 25484), ("Social Media Mgt", 47205),
+           ("EIRP Honors", 64284)]
+
+app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template("index.html", courses = courses)
+
+@app.route("/add_course")
+def add_course():
+    return render_template("index.html")
+
+
+@app.route("/register_student", methods = ['GET, 'POST'])
+def register_student():
+    return render_template(" ")
--- playground/models.py
+++ playground/models.py
@@ -1,11 +1,24 @@
 from app import db
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+import os
+import csv
+import sys
+
+db = SQLAlchemy(app)
+basedir = os.path.abspath(os.path.dirname(__file__))
+DATABASE_URL = 'sqlite:///' + os.path.join(basedir,'app.db')
 
 class Course(db.Model):
+    __tablename__ = "Course Names"
     id = db.Column(db.Integer, primary_key = True)
-    course_number = db.Column(db.String(30),index = True, unique = True)
-    course_title = db.Column(db.String(30), index = True, unique = True)
+    course_number = db.Column(db.String,index = True, unique = True)
+    course_title = db.Column(db.String, index = True, unique = True)
 
 class RegisteredStudent(db.Model):
+    __tablename__ = "Registered Students"
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), index = True, unique = True)
-    grade = db.Column(db.String(3), index = True, unique = True)
+    name = db.Column(db.String, index = True, unique = True)
+    grade = db.Column(db.String, index = True, unique = True)
